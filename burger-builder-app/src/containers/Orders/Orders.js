747e82ce24c488b80233f614ab981258775cce32
Paco <franfercant@gmail.com>
2019-04-03 22:19:23 +0200

    orders fetched

--- burger-builder-app/src/containers/Orders/Orders.js
+++ burger-builder-app/src/containers/Orders/Orders.js
@@ -2,9 +2,32 @@ import React, {Component} from 'react'
 
 import Order from '../../components/Order/Order'
 
+import axios from '../../axios-orders'
+
+import withErrorHandler from '../../hoc/WhitErrorHandler/whitErrorHandler'
+
 class Orders extends Component {
 
     state = {
+        Orders: [],
+        Loading: true
+    }
+
+    componentDidMount () {
+        axios.get('/orders.json')
+            .then (res=>{
+                const fetchedOrders = []
+                for (let key in res.data) {
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    })
+                }
+                this.setState({Loading:false, Orders: fetchedOrders})
+            })
+            .catch (err => {
+                this.setState({Loading:false})
+            })
 
     }
 
@@ -19,4 +42,4 @@ class Orders extends Component {
 
 }
 
-export default Orders
+export default withErrorHandler(Orders, axios)
